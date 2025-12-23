4bf526f5e7f10433481270d4b592cb34807a02e9
RSTurgay <turgay@realsolutions.com.tr>
2025-12-23 19:51:36 +0300

    TBSTR-2016

--- app/src/main/java/com/realsolutions/rsuikit/MainActivity.java
+++ app/src/main/java/com/realsolutions/rsuikit/MainActivity.java
@@ -10,11 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.realsolutions.uikit.RSButton;
 import com.realsolutions.uikit.RSEditText;
+import com.realsolutions.uikit.RSPasswordInput;
 
 public class MainActivity extends AppCompatActivity {
 
     RSEditText rsEditText;
     RSButton btnPrimary, btnSecondary;
+    RSPasswordInput inpPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +24,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         rsEditText = findViewById(R.id.inpSicil);
+        inpPass = findViewById(R.id.inpPass);
         btnPrimary = findViewById(R.id.btnPrimary);
         btnSecondary = findViewById(R.id.btnSecondary);
 
         btnPrimary.setOnClickListener(view -> {
             rsEditText.setErrorState(null);
+            inpPass.setErrorState();
         });
 
         btnSecondary.setOnClickListener(view -> {
             rsEditText.setSuccessState();
+            inpPass.setSuccessState();
         });
     }
 }
